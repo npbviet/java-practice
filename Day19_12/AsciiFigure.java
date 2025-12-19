cbfaf43e2ae2d95c1c314a9c4f296b2c95f22649
Test P <npb.viet@gmail.com>
2025-12-19 19:36:25 +0700

    day 19/12

---
+++ Day19_12/AsciiFigure.java
@@ -0,0 +1,16 @@
+public class AsciiFigure {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+        drawFigure();
+    }
+     public static void drawFigure() {
+        for (int i=1; i<= SIZE; i++) {
+            for (int j=1; j<= 4* (SIZE-1); j++) {
+
+            }
+            System.out.println();
+            
+        }
+     }
+
+}
