4c9b6d5134ebdc655160812f824ccde9e05dea41
Test P <npb.viet@gmail.com>
2025-12-19 20:08:00 +0700

    Fix: variable size

--- Day19_12/AsciiFigure.java
+++ Day19_12/AsciiFigure.java
@@ -3,14 +3,23 @@ public class AsciiFigure {
         // System.out.println("Hello");
         drawFigure();
     }
-     public static void drawFigure() {
-        for (int i=1; i<= SIZE; i++) {
-            for (int j=1; j<= 4* (SIZE-1); j++) {
 
+    public static final int SIZE = 7;
+
+    public static void drawFigure() {
+        for (int i=1; i <= SIZE; i++) {
+            for (int j=1; j <= 4 * (SIZE - i); j++) {
+                System.out.print("/");
+            }
+            for (int j=1; j <= 8 * (i - 1); j++) {
+                System.out.print("*");
+            }
+            for (int j=1; j <= 4 * (SIZE - i); j++) {
+                System.out.print("\\");
             }
             System.out.println();
             
         }
-     }
+    }
 
 }
