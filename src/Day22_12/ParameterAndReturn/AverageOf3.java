2bc3f4a7c40dbfbe51dbf0da5962e14e7bb718ca
Test P <npb.viet@gmail.com>
2025-12-22 17:04:09 +0700

    Feat:add Exercise of Day 22_12

---
+++ src/Day22_12/ParameterAndReturn/AverageOf3.java
@@ -0,0 +1,9 @@
+public class AverageOf3 {
+    public static double tinhTrungBinh(int x, int y, int z) {
+        return (x + y + z) / 3.0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Trung bình cộng là: " + tinhTrungBinh(1, 2, 3));
+        System.out.println("Trung bình cộng là: " + tinhTrungBinh(4, 7, 13));
+    }
+}
---
+++ src/Day22_12/ParameterAndReturn/BinaryToDecimal.java
@@ -0,0 +1,27 @@
+package Day22_12.ParameterAndReturn;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        System.out.println("Số chuyển đổi là: " + decimalOf(101011));
+    }
+
+    public static double decimalOf(int binaryNum) {
+        int decimalNum = 0;
+        int x = 0;
+        while (binaryNum > 0) {
+            int y = binaryNum%10;
+            decimalNum += y*Math.pow(2, x);
+//            System.out.println("Số hiện tại: " + decimalNum);
+            binaryNum = binaryNum/10;
+            x++;
+//            System.out.println("Số lần thực hiện: " + x);
+        }
+        return decimalNum;
+    }
+}
+
+//Cách thực hiện: Khai báo int để lấy số nguyên
+// Lấy số chia 10 lấy dư để lấy số ngoài cùng.
+// Sau mỗi lần thực hiện thì chia cho 10, loại bỏ phần vừa tính
+// Lấy x là số lần tính, y là ngoài cùng, tính y*2^x
+// Lấy decimalNum + kết quả vừa tính, kết quả cuối cùng là số cần tìm
